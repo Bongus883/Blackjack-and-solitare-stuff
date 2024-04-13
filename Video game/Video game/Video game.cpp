a9961962f7b8888452feadd3bd3128420300b3de
Bongus883 <153528156+Bongus883@users.noreply.github.com>
2024-04-13 17:36:59 +0100

    Comments for the future
    Basically everything for blackjack that doesn't involve coding has been written down, so that I can use it later for when everything will be more complete

--- Video game/Video game/Video game.cpp
+++ Video game/Video game/Video game.cpp
@@ -26,8 +26,16 @@ int main()
 	cout << "" << endl;
 	//cout << "The dealer got: " 
 	cout << "Your current total is " + YOU.get_card_value_P() << endl;
-	//cout << "The dealer's current total is " 
+	//cout << "The dealer's current total is _" 
 	//cout << "Do you want to hit or stand?"
 	//cin >> 
-
+	//You decided to hit. You got: _
+	//You decided to stand
+	//Your final total is _
+	//The dealer also had: _ meaning that their final total is _
+	//You went bust... The entire casino collectively stands up, looks in your direction, and begins pointing and laughing at you. The dealer then begins to ask you which of many debilitating neurological symptoms you are currently suffering from, seeing as thought they would very clearly be the cause of your loss in this luck based game. So, what now? You aren't the type to be easily dissuaded, and your addictive vices won't feed themselves. Do you want to play again despite your shameful defeat? (Type yes or no)
+	//YOU WON!! You leap up onto the table and begin breakdancing in the dealer's face as they being foaming at the mouth. You actually have no idea how much money you won, but you don't care. The sweet, sweet taste of victory and the disturbing gargling noises coming from the dealer's convulsing body are more than enough for you. As the dealer is carried away on a stretcher while a new one walks up to the table to replace them, you wonder what you are going to do now. Do you want to end on a high and take your leave now, or do you want to risk it all and play another game? (Type yes to play again, or no to stop)
+	//cin >>
+	//You decide that enough is enough, and proceed to scuttle out of the casino the way you came in, refusing to elaborate to the security guards as to why you came into the casino completely naked. Thank you for playing!
+	//You strap yourself in and prepare yourself for another round... The dealer calls you a degenerate schmuck, before spitting in your face again and dealing the ''''cards''''
 }
