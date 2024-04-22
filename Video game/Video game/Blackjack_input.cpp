93dea0cda26b25b87703c145bdc12875d716ef63
Bongus883 <153528156+Bongus883@users.noreply.github.com>
2024-04-22 13:37:04 +0100

    ?
    What? When did I do this??

--- Video game/Video game/Blackjack_input.cpp
+++ Video game/Video game/Blackjack_input.cpp
@@ -2,18 +2,18 @@
 #include "Deck.h"
 #include "Player.h"
 
-void Blackjack_input::Hit(string Hit)
-{
-}
+//void Blackjack_input::Hit(string Hit)
+//{
+//}
 
-void Blackjack_input::Stand(string Stand)
-{
-}
+//void Blackjack_input::Stand(string Stand)
+//{
+//}
 
-void Blackjack_input::Play_again(string Yes)
-{
-}
+//void Blackjack_input::Play_again(string Yes)
+//{
+//}
 
-void Blackjack_input::Stop_game(string No)
-{
-}
+//void Blackjack_input::Stop_game(string No)
+//{
+//}
