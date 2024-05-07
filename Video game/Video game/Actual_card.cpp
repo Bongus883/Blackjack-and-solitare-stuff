4ade48360c1e2159b751629c8cd1ccf0e8569e74
Bongus883 <zebguppy@gmail.com>
2024-05-07 22:02:34 +0100

    I am the best programmer to ever live
    Yeah that's right

--- Video game/Video game/Actual_card.cpp
+++ Video game/Video game/Actual_card.cpp
@@ -1,13 +1,18 @@
 #include "Actual_card.h"
 
-Actual_card::Actual_card(int numbero, string other_suit, string funny) {
+Actual_card::Actual_card(int numbero, int one, string other_suit, string funny) {
 	Numbero = numbero;
+	One = one;
 	Other_suit = other_suit;
 	Funny = funny;
 }
 
 int Actual_card::get_the_number() {
 	return Numbero;
+}
+
+int Actual_card::embezzlement_total() {
+	return One;
 }
 
 string Actual_card::get_other_suit() {
--- Video game/Video game/Actual_card.h
+++ Video game/Video game/Actual_card.h
@@ -7,12 +7,14 @@ class Actual_card
 {
 private:
 	int Numbero;
+	int One;
 	string Other_suit;
 	string Funny;
 
 public:
-	Actual_card(int numbero, string other_suit, string funny);
+	Actual_card(int numbero, int one, string other_suit, string funny);
 	int get_the_number();
+	int embezzlement_total();
 	string get_other_suit();
 	string get_funny_thing();
 };
